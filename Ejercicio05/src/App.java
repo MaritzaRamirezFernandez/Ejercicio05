f8083f88012d5fe4906f525c32da6498b30297d0
Maritza <Maritza@Maritza-PC>
2016-01-29 14:58:49 -0500

    3

--- Ejercicio05/src/App.java
+++ Ejercicio05/src/App.java
@@ -15,6 +15,7 @@ public class App {
      */
     public static void main(String[] args) {
         System.out.println("Linux "); 
-        System.out.println("linux 2");}
+        System.out.println("linux 2");//:D 
+    }
     
 }
